860450f86a5bd698229988c4e8deccbc68b611ab
Turzo Ahsan Sami <turzoahsansami@gmail.com>
2020-07-10 00:02:17 +0600

    .

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Turzone Next</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
